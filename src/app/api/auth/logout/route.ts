28fa082f3a0bd492594e0c2887ccd9e74b3a896f
RIITK SINGH <businessritiksinghdeos@gmail.com>
2026-06-07 12:02:26 +0530

    Project Completed

---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,11 @@
+
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+    const cookieStore = await cookies()
+    cookieStore.delete("access_token")
+    return NextResponse.json(`${process.env.NEXT_PUBLIC_APP_URL}`)
+
+}
+
---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,102 @@
+
+
+import { NextRequest, NextResponse } from 'next/server'
+import Settings from '@/model/settings.model'
+import { GoogleGenAI } from '@google/genai'
+import connectDb from '@/lib/db'
+
+export async function POST(req: NextRequest) {
+    try {
+
+
+        const { message, ownerId } = await req.json()
+
+        if (!message || !ownerId) {
+            return NextResponse.json(
+                { message: "message and owner id is required" },
+                { status: 400 }
+            )
+        }
+        await connectDb()
+        const setting = await Settings.findOne({ ownerId })
+
+        if (!setting) {
+            return NextResponse.json(
+                { message: "Chatbot is not configured yet" },
+                { status: 400 }
+            )
+        }
+
+        const KNOWLEDGE = `
+        business name- ${setting.businessName || "not provided"} 
+        support email- ${setting.supportEmail || "not provided"}
+        knowledge- ${setting.knowledge || "not provided"}
+        `
+
+        const prompt = `
+        You are a professional customer support assistant for this business.
+        Use ONLY the information provided below to answer the customer's question.
+        You may rephrase, summarise, or interpret the information if needed,
+        Do Not invent new policies, prices, or promises.
+        
+        If the customer greets you with messages like:
+       "hi", "hello", "hey", "good morning", or similar greetings,
+        respond warmly and introduce yourself as the support assistant.
+
+        Example:
+        "Hello! Welcome to ${setting.businessName} support. How can I assist you today? Feel free to ask me anything about our products, services, or store information."
+
+        If the customer's question is completely unrelated to the business information,
+        reply exactly with:
+        "Please contact support."
+        
+        -----------------------
+        BUSINESS INFORMATION
+        -----------------------
+        ${KNOWLEDGE}
+        -----------------------
+        CUSTOMER QUESTION:
+        -----------------------
+        ${message}
+        -----------------------
+        ANSWER
+        -----------------------
+        `;
+
+        const ai = new GoogleGenAI({
+            apiKey: process.env.GEMINI_API_KEY
+        });
+
+        const res = await ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: prompt,
+        });
+
+        const response = NextResponse.json(res.text)
+        response.headers.set("Access-Control-Allow-Origin", "*");
+        response.headers.set("Access-Control-Allow-Methods", "POST, OPTIONS");
+        response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+        return response
+
+    } catch (error) {
+        const response = NextResponse.json(
+            { message: `Chat error ${error}` },
+            { status: 500 }
+        )
+        response.headers.set("Access-Control-Allow-Origin", "*");
+        response.headers.set("Access-Control-Allow-Methods", "POST, OPTIONS");
+        response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+        return response
+    }
+}
+``
+export const OPTIONS = async () => {
+    return new NextResponse(null, {
+        status: 201,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type",
+        },
+    });
+}
